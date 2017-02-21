bbc35cdd7d9630ac79062d59aeae69a0cc2e3adc
AntonSmyrnoff <a.smyrnoff@gmail.com>
2017-02-21 17:45:59 +0200

    pictures onload are done

--- js/script.js
+++ js/script.js
@@ -1,36 +1,47 @@
 $(function () {
 
-        /*$('.grid').isotope({
-          itemSelector: '.grid-item',
-          masonry: {
-            columnWidth: 300
-          }
-        });*/
+      
+      var arr = ['sport', 'city', 'ocean', 'sky', 'nature', 'beauty', 'food', 'computer', 'science', 'technology', 'animal', 
+                  'winter', 'summer', 'autumn', 'river', 'mountain', 'office', 'new york', 'waterfall', 'africa', 'america', 
+                  'castle', 'horse', 'children', 'school', 'snow', 'sun', 'space'];
+      
+      for (var i=0; i<7; i++) {
+      var rand = Math.floor(Math.random()*arr.length);
+      var arrDiv = [];
+       
+       $.ajax({
+             url: "https://pixabay.com/api/?key=3981741-7a8eb8522a5415851535e06c2&q="+arr[rand]+"&image_type=photo&per_page=10",
+             dataType : "jsonp",
+             success: function(data) {
+                var div = document.createElement("div");
 
-    var arr = [];
-    var response = $.ajax({
-       url: "http://api.pixplorer.co.uk/image?amount=7&size=tb",
-       dataType : "JSON",
-       success: function(data) {
-        console.log(data);
-        $.each(data.images, function(i, val){
-                            var div = document.createElement("div");
+                div.classList.add('grid-item');
+                div.style.background = 'url('+data.hits[Math.floor(Math.random()*10)].previewURL+') 50% no-repeat';
+                $('#wrapper').append(div);
+                arrDiv.push(div);
 
-                            div.classList.add('grid-item');
-                            if(i==4||i==5) {
-                                div.classList.add('grid-item--width2') 
-                            };
-                            div.style.background = 'url('+data.images[i].imageurl+') no-repeat 50%';
-                            $('#wrapper').append(div);
-                          
-                        });
+                if (arrDiv.length == 7) {
+                  $('.grid-item:eq(4)').addClass('grid-item--width2');
+                  $('.grid-item:eq(5)').addClass('grid-item--width2');
+                  $('.grid').isotope({
+                              itemSelector: '.grid-item',
+                              percentPosition: true,
+                              masonry: {
+                                columnWidth: '.grid-item',
+                              }
+                            });
+                  $('.grid-item').css({'right': 0, 'background-size': 'cover'});
+                }
 
-       }
-    });
+              },
+          });
+     }
+     
 
 
-    
+
 });
+
 
 
 
