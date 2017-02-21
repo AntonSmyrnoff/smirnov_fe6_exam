a78a24023bdb55cfa86c767d5ea6a76bc74d4764
Anton Smyrnov <a.smyrnoff@gmail.com>
2017-02-21 23:42:51 +0200

    footer is started

--- js/script.js
+++ js/script.js
@@ -7,20 +7,28 @@ $(function () {
       
       for (var i=0; i<7; i++) {
       var rand = Math.floor(Math.random()*arr.length);
-      var arrDiv = [];
+      var count = 0;
        
        $.ajax({
              url: "https://pixabay.com/api/?key=3981741-7a8eb8522a5415851535e06c2&q="+arr[rand]+"&image_type=photo&per_page=10",
              dataType : "jsonp",
              success: function(data) {
                 var div = document.createElement("div");
+                var randomItem = data.hits[Math.floor(Math.random()*10)];
 
                 div.classList.add('grid-item');
-                div.style.background = 'url('+data.hits[Math.floor(Math.random()*10)].previewURL+') 50% no-repeat';
+                div.style.background = 'url('+randomItem.previewURL+') 50% no-repeat';
+                
+                var a = document.createElement("a");
+                a.setAttribute('href', '#');
+                a.innerHTML = randomItem.tags;
+                div.append(a);
                 $('#wrapper').append(div);
-                arrDiv.push(div);
 
-                if (arrDiv.length == 7) {
+
+                count+=1;
+
+                if (count == 7) {
                   $('.grid-item:eq(4)').addClass('grid-item--width2');
                   $('.grid-item:eq(5)').addClass('grid-item--width2');
                   $('.grid').isotope({
